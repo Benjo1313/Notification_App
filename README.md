84716a34408069f685bc8a4f1d18f7a28dd63558
Ben Kaul <bkaul1313@gmail.com>
2025-10-27 19:59:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_notif_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_notif_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_notif_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
